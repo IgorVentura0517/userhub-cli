93158858da4d22956611238b73b97a40e3031842
Igor Ventura <igorventura1705@gmail.com>
2026-03-19 11:25:25 -0300

    feat: adiciona interface CLI com opções de cadastro e listagem de usuários

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from database import create_table, create_user, get_users
 
 
+def menu():
+    while True:
+        print("1- Cadastrar usuário")
+        print("2 - Listar usuários")
+        print("3 - Sair")
+
+        opcao = input("Digite sua opção: ")
+
+        if opcao == "1":
+            nome = input("Digite seu nome: ")
+            email = input("Digite seu email: ")
+            create_user(nome,email)
+        
+        elif opcao == "2":
+            users = get_users()
+            for user in users:
+                print(f"{user[0]} - {user[1]} - {user[2]}")
+
+        elif opcao == "3":
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida!")
+
+
 if __name__ == "__main__":
     create_table()
-
-
-create_user("Igor", "igor@email.com")
-
-users = get_users()
-print(users)
+    menu()
